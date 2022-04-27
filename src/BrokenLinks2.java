aabd1054086bf0a804c3fc8d83ca765b4ea320bc
Olukoya <Yemi.Olukoya@hcahealthcare.co.uk>
2022-04-27 23:26:18 +0100

    update

---
+++ src/BrokenLinks2.java
@@ -0,0 +1,66 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrokenLinks2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\yolukoya\\MyWorkSpace\\SeleniumLink\\chromedriver.exe");
+	     WebDriver driver=new ChromeDriver();
+	     
+//			WebDriver driver=new FirefoxDriver();
+//			
+			driver.manage().window().maximize();
+			
+			driver.get("https://www.hcahealthcare.co.uk/");
+			Thread.sleep(5000);
+
+		List<WebElement> linksList = clickableLinks(driver);
+		for (WebElement link : linksList) {
+			String href = link.getAttribute("href");
+			try {
+				System.out.println("URL " + href + " returned " + linkStatus(new URL(href)));
+			}
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
+	}
+	
+	public static List<WebElement> clickableLinks(WebDriver driver) {
+		List<WebElement> linksToClick = new ArrayList<WebElement>();
+		List<WebElement> elements = driver.findElements(By.tagName("a"));
+		elements.addAll(driver.findElements(By.tagName("img")));
+		
+		for (WebElement e : elements) {
+			if (e.getAttribute("href") != null) {
+				linksToClick.add(e);
+			}
+		}
+		return linksToClick;
+	}
+	
+	public static String linkStatus(URL url) {
+		// http://download.java.net/jdk7/archive/b123/docs/api/java/net/HttpURLConnection.html#getResponseMessage%28%29
+		try {
+			HttpURLConnection http = (HttpURLConnection) url.openConnection();
+			http.connect();
+			String responseMessage = http.getResponseMessage();
+			http.disconnect();
+			return responseMessage;
+		}
+		catch (Exception e) {
+			return e.getMessage();
+		}
+	}
+	}
+
+
